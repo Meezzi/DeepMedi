4da08c42bb86d099fedca0a1f0432d11f09e43ae
Meezzi <alswl-3016@naver.com>
2025-02-16 21:38:51 +0900

    feat: 촬영 버튼 구현

--- app/src/main/java/com/meezzi/deepmedi/presentation/ui/camera/CameraScreen.kt
+++ app/src/main/java/com/meezzi/deepmedi/presentation/ui/camera/CameraScreen.kt
@@ -1,18 +1,30 @@
 package com.meezzi.deepmedi.presentation.ui.camera
 
 import android.content.Context
+import android.widget.Toast
 import androidx.camera.core.CameraSelector
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.LifecycleOwner
+import com.meezzi.deepmedi.R
 
 @Composable
 fun CameraScreen(
@@ -25,6 +37,46 @@ fun CameraScreen(
 
     Box(modifier = Modifier.fillMaxSize()) {
         CameraPreview(cameraController)
+
+        Column(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(80.dp)
+        ) {
+            CaptureButton(
+                onclick = {
+                    cameraViewModel.captureImage(
+                        cameraController = cameraController,
+                        onSuccess = { onNavigateToResult() },
+                        onError = {
+                            Toast.makeText(
+                                context,
+                                context.getString(R.string.camera_error_message),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    )
+                },
+            )
+        }
+    }
+}
+
+@Composable
+private fun CaptureButton(
+    onclick: () -> Unit,
+) {
+    IconButton(
+        onClick = { onclick() },
+        modifier = Modifier
+            .size(70.dp),
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_camera_button),
+            contentDescription = stringResource(id = R.string.camera),
+            modifier = Modifier.size(200.dp),
+            tint = Color.Unspecified
+        )
     }
 }
 
