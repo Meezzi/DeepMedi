4915c8d97a900baa287211e2502f8e2d2c3782e6
Meezzi <alswl-3016@naver.com>
2025-02-17 06:17:14 +0900

    feat: 사용자 속성을 가져오는 API 메서드 추가

--- app/src/main/java/com/meezzi/deepmedi/data/api/ApiService.kt
+++ app/src/main/java/com/meezzi/deepmedi/data/api/ApiService.kt
@@ -3,6 +3,7 @@ package com.meezzi.deepmedi.data.api
 import com.meezzi.deepmedi.data.model.AuthRequest
 import com.meezzi.deepmedi.data.model.AuthResponse
 import com.meezzi.deepmedi.data.model.ImageUploadResponse
+import com.meezzi.deepmedi.data.model.UserAttribute
 import com.meezzi.deepmedi.data.model.UserInfoResponse
 import okhttp3.MultipartBody
 import retrofit2.Response
@@ -12,6 +13,7 @@ import retrofit2.http.Header
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
+import retrofit2.http.Path
 
 interface ApiService {
 
@@ -30,4 +32,10 @@ interface ApiService {
     suspend fun fetchUserInfo(
         @Header("Authorization") authToken: String
     ): Response<UserInfoResponse>
+
+    @GET("api/plugins/telemetry/USER/{user_id}/values/attributes/SERVER_SCOPE")
+    suspend fun fetchUserAttributes(
+        @Path("user_id") userId: String,
+        @Header("Authorization") authToken: String
+    ): Response<List<UserAttribute>>
 }
