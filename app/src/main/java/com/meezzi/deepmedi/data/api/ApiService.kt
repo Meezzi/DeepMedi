bb4f5661d86a77809b7ccca8603f1a6fa4dd7712
Meezzi <alswl-3016@naver.com>
2025-02-17 06:03:48 +0900

    feat: 사용자 인증 API 메서드 추가

--- app/src/main/java/com/meezzi/deepmedi/data/api/ApiService.kt
+++ app/src/main/java/com/meezzi/deepmedi/data/api/ApiService.kt
@@ -1,8 +1,11 @@
 package com.meezzi.deepmedi.data.api
 
+import com.meezzi.deepmedi.data.model.AuthRequest
+import com.meezzi.deepmedi.data.model.AuthResponse
 import com.meezzi.deepmedi.data.model.ImageUploadResponse
 import okhttp3.MultipartBody
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
@@ -14,4 +17,9 @@ interface ApiService {
     suspend fun uploadImage(
         @Part image: MultipartBody.Part
     ): Response<ImageUploadResponse>
+
+    @POST("api/auth/login")
+    suspend fun authenticateUser(
+        @Body loginRequest: AuthRequest
+    ): Response<AuthResponse>
 }
