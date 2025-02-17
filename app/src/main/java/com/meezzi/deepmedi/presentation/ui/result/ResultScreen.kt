f58ca2f5bfbefdbdb6927ff3c411aeca9e564516
Meezzi <alswl-3016@naver.com>
2025-02-17 12:38:40 +0900

    feat: 건강 상태 색깔 정의

--- app/src/main/java/com/meezzi/deepmedi/presentation/ui/result/ResultScreen.kt
+++ app/src/main/java/com/meezzi/deepmedi/presentation/ui/result/ResultScreen.kt
@@ -3,6 +3,32 @@ package com.meezzi.deepmedi.presentation.ui.result
 import androidx.compose.runtime.Composable
 
 @Composable
-fun ResultScreen() {
+fun StatusTag(status: String, backgroundColor: Color, textColor: Color = Color.Black) {
+    Box(
+        modifier = Modifier
+            .background(
+                backgroundColor,
+                shape = RoundedCornerShape(50)
+            )
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .wrapContentSize()
+    ) {
+        Text(
+            text = status,
+            color = textColor,
+            style = MaterialTheme.typography.bodyMedium,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
 
+fun getStatusColor(status: String): Color {
+    return when (status) {
+        "위험" -> Color.Red
+        "경고" -> Color.Magenta
+        "관심" -> Color.Yellow
+        "정상" -> Color.Cyan
+        "건강" -> Color.Blue
+        else -> Color.Gray
+    }
 }
